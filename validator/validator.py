41aa8648fdd5e5166b85fe1f0189ed7abf5b62e5
miguel.pimenta <pimenta.mig@gmail.com>
2022-06-04 20:13:03 +0100

    fix workflow

--- validator/validator.py
+++ validator/validator.py
@@ -3,7 +3,7 @@ import yaml
 
 
 def load_doc():
-    with open('../data/domain.yml', 'r') as stream:
+    with open('./data/domain.yml', 'r') as stream:
         try:
             return yaml.safe_load(stream)
         except yaml.YAMLError as exception:
